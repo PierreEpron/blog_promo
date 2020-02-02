44a1edfcf334775d99823f11ccccade418c0b472
= <=>
2020-02-02 15:18:20 +0100

    Make blog.home view with Article model

--- blog_promo/blog/views.py
+++ blog_promo/blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Article
 
 def home(request):
-    return render(request, 'blog/home.html')
+    return render(request, 'blog/home.html', {'articles' : Article.get_with_shape((3,3))})
