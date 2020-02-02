e487d32307eaf2268b68463354c2b051b25c7e53
= <=>
2020-02-02 11:50:36 +0100

    Add Article Model

--- blog_promo/blog/models.py
+++ blog_promo/blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.conf import settings
+from django.contrib.auth import get_user_model
+
+def get_sentinel_user():
+    return get_user_model().objects.get_or_create(username='deleted')[0]
 
 # Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=256)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET(get_sentinel_user))
+    content = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField('last time modified', auto_now=True)
